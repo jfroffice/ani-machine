5c76ac750e5b02cf5aeab64b5e44caaedf162533
jfroffice <jfroffice@gmail.com>
2014-10-15 23:35:54 +0200

    update build version

--- dist/ani-machine.js
+++ dist/ani-machine.js
@@ -1,6 +1,6 @@
 /**
  * ani-machine - Declarative animation and machine state
- * @version v0.1.2
+ * @version v0.1.3
  * @link https://github.com/jfroffice/ani-machine
  * @license MIT
  */
@@ -225,9 +225,7 @@ var enter = (function() {
 		};
 	}
 
-	return {
-		genCSS: genCSS
-	};
+	return { genCSS: genCSS };
 
 })();
 var animator = (function() {
@@ -292,10 +290,154 @@ var animator = (function() {
 	};
 
 })();
+var am = am || {};
+am.maestro = {
+	init: function(options) {
+
+		var self = this,
+			triggers = options.triggers;
+
+		self.events = options.events;
+		self.element = options.element;
+		self.deferFn = options.timeoutFn;
+		self.jobs = [];
+		self.unregisters = [];
+		self.currentState;
+		self.running;
+
+		function initTrigger(state, trigger) {
+			var tmp = trigger.split(' '),
+				selector = tmp[0],
+				on = tt.parseOn(tmp[1]);
+
+			[].forEach.call(document.querySelectorAll(selector), function(el) {
+				el.addEventListener(on, function() {
+					self.changeState(state);
+				});
+			});
+		}
+
+		for(var state in triggers) {
+			var trigger = triggers[state];
+			if (!trigger) {
+				continue;
+			}
+			initTrigger(state, trigger);
+		}
+	},
+	changeState: function(state) {
+
+		var self = this,
+			ACTIVE = 'active';
+
+		function addQueue(job) {
+			self.jobs.push(job);
+			run();
+		}
+
+		function run() {
+			if (self.running) {
+				return;
+			}
+			var job = self.jobs[0];
+			if (!job) {
+				return;
+			}
+			self.running = true;
+			job.run(function() {
+				job.finish();
+				self.jobs.splice(0, 1);
+				self.running = false;
+				run();
+			});
+		}
+
+		function initEvents(events) {
+			if (!events) {
+				return;
+			}
+
+			var tmp = [];
+			events.forEach(function(e) {
+				tmp.push(initEvent(e));
+			});
+			return tmp;
+		}
+
+		function initEvent(event) {
+			var goto = event.goto,
+				on = event.on;
+
+			function eventFn() {
+
+				var params = event.param.split(' ');
+				
+				if (params && params[0] === ':enter') {
+					addQueue({
+						run: animator.build(self.element, 'enter', params.slice(1, params.length)),
+						finish: finish
+					});	
+				} else {
+					event.currentStep += 1;
+					if (event.currentStep >= params.length) {
+						console.warn('try to relaunch animation that is not finished');
+					} else {
+						addQueue({
+							run: animator.build(self.element, event.type, params[event.currentStep]),
+							finish: finishSequence
+						});	
+					}												
+				}
+			};
+
+			function finish() {
+				if (goto) {
+					if (on !== ACTIVE) {
+						self.element.off(on, eventFn);
+					}
+					changeState(goto);
+				}
+			}
+
+			function finishSequence() {
+				if (event.currentStep < (event.param.split(' ').length-1)) {
+					self.deferFn(eventFn, 0);
+				} else {
+					event.currentStep = -1;
+				}
+
+				finish();
+			}
+
+			if (on === ACTIVE) { // autostart animation
+				self.deferFn(eventFn, 0);
+			} else {
+				self.element.on(on, eventFn);
+			}
+		
+			return function() {
+				self.element.off(on, eventFn);
+			};
+		}
+
+		var sameState = self.currentState === state;
+
+		if (self.currentState && self.unregisters && !sameState) {
+			for (var i=0; i<self.unregisters.length; i++) {
+				self.unregisters[i]();
+			}
+		}
+
+		if (sameState) {
+			initEvents(self.events[state]);
+		} else {
+			self.currentState = state;
+			self.unregisters = initEvents(self.events[state]);
+		}
+	}
+};
 angular.module('aniMachine', [])
 .directive('amElement', ['$timeout', '$window', function($timeout, $window) {
-
-	var ACTIVE = 'active';
 
 	return {
 		restrict: 'A',
@@ -305,149 +447,21 @@ angular.module('aniMachine', [])
 		link: function(scope, element, options) {
 
 			var events = scope.events,
-				triggers = scope.triggers,
-				timeoutFn = $timeout,
-				jobs = [],
-				running,
-				unregisters,
-				currentState;
+				triggers = scope.triggers;
 
-			function changeState(state) {
-				var sameState = currentState === state;
-
-				if (currentState && unregisters && !sameState) {
-					for (var i=0; i<unregisters.length; i++) {
-						unregisters[i]();
-					}
-				}
-
-				if (sameState) {
-					initEvents(events[state]);
-				} else {
-					currentState = state;
-					unregisters = initEvents(events[state]);
-				}
-			}
-
-			function initTriggers() {
-				for(var state in triggers) {
-					var trigger = triggers[state];
-					if (!trigger) {
-						continue;
-					}
-					initTrigger(state, trigger);
-				}
-			}
-
-			function initTrigger(state, trigger) {
-				var tmp = trigger.split(' '),
-					selector = tmp[0],
-					on = tt.parseOn(tmp[1]);
-
-				[].forEach.call(document.querySelectorAll(selector), function(el) {
-					el.addEventListener(on, function() {
-						changeState(state);
-					});
-				});
-			}
-
-			function initEvents(events) {
-				if (!events) {
-					return;
-				}
-
-				var tmp = [];
-				events.forEach(function(e) {
-					tmp.push(initEvent(e));
-				});
-				return tmp;
-			}
-
-			function addQueue(job) {
-				jobs.push(job);
-				run();
-			}
-
-			function run() {
-				if (running) {
-					return;
-				}
-				var job = jobs[0];
-				if (!job) {
-					return;
-				}
-				running = true;
-				job.run(function() {
-					job.finish();
-					jobs.splice(0, 1);
-					running = false;
-					run();
-				});
-			}
-
-			function initEvent(event) {
-				var goto = event.goto,
-					on = event.on;
-
-				function eventFn() {
-
-					var params = event.param.split(' ');
-					
-					if (params && params[0] === ':enter') {
-						addQueue({
-							run: animator.build(element, 'enter', params.slice(1, params.length)),
-							finish: finish
-						});	
-					} else {
-						event.currentStep += 1;
-						if (event.currentStep >= params.length) {
-							console.warn('try to relaunch animation that is not finished');
-						} else {
-							addQueue({
-								run: animator.build(element, event.type, params[event.currentStep]),
-								finish: finishSequence
-							});	
-						}												
-					}
-				};
-
-				function finish() {
-					if (goto) {
-						if (on !== ACTIVE) {
-							element.off(on, eventFn);
-						}
-						changeState(goto);
-					}
-				}
-
-				function finishSequence() {
-					if (event.currentStep < (event.param.split(' ').length-1)) {
-						$timeout(eventFn, 0);
-					} else {
-						event.currentStep = -1;
-					}
-
-					finish();
-				}
-
-				if (on === ACTIVE) { // autostart animation
-					timeoutFn(eventFn, 0);
-				} else {
-					element.on(on, eventFn);
-				}
-			
-				return function() {
-					element.off(on, eventFn);
-				};
-			}
-
-			initTriggers();
+			var musician = Object.create(am.maestro);
+			musician.init({
+				triggers: triggers,
+				events: events,
+				element: element,
+				timeoutFn: $timeout
+			});			
 
 			if (events.enter || events.leave) {
 
 				if (am.viewport.isInside(element[0])) {
 					if (!events['default']) {
-						changeState('enter');
+						musician.changeState('enter');
 					}
 				}
 
@@ -455,9 +469,9 @@ angular.module('aniMachine', [])
 						return am.viewport.isInside(element[0]);
 					}, function(newValue, oldValue) {
 						if (newValue !== oldValue) {
-							changeState(newValue ? 'enter' : 'leave');
+							musician.changeState(newValue ? 'enter' : 'leave');
 					   }
-				}, true);
+					}, true);
 
 				// should be outside !?
 				angular.element($window)
@@ -469,17 +483,7 @@ angular.module('aniMachine', [])
 					});
 			}
 
-			if (!options.enter) {
-				changeState('default');
-				return;
-			}
-
-			addQueue({
-				run: animator.build(element, 'enter', options.enter),
-				finish: function() {
-					changeState('default');
-				}
-			});
+			musician.changeState('default');
 		},
 		controller: ['$scope', '$element', function($scope, $element) {
 
